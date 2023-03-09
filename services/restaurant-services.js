8316c1f9d8f3f3c7c0b96444c98ff037f1251f0f
sd880428 <sd660428@gmail.com>
2023-03-09 23:33:47 +0800

    feat: add restaurant-services

---
+++ services/restaurant-services.js
@@ -0,0 +1,44 @@
+const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+
+const restaurantController = {
+  getRestaurants: (req, cb) => {
+    const DEFAULT_LIMIT = 9
+    const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        offset,
+        limit,
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const likedRestaurants = req.user?.LikedRestaurants?.map(lr => lr.id) || []
+        const favoritedRestaurantsId = req.user?.FavoritedRestaurants?.map(fr => fr.id) || []
+        const data = restaurants.rows.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id),
+          isLiked: likedRestaurants.includes(r.id)
+        }))
+        return cb(null, {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
+      })
+      .catch(err => cb(err))
+  }
+}
+
+module.exports = restaurantController
